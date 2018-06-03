4dca9fa991994c0efdbfbaea171c245ec6eb7735
LuisAlfredoMtz <luisalfredomtz@hotmail.com>
2018-06-03 10:36:06 -0500

    split sprint

--- services/web/src/app/assets/app/components/App/App.jsx
+++ services/web/src/app/assets/app/components/App/App.jsx
@@ -5,6 +5,22 @@ import Card from '../Card';
 import Login from '../Login';
 import Sprint from '../Sprint';
 
+function CardItem(props) {
+  return (<Sprint key={props.id} name={props.name} date="February 20">
+      {props.userStories.map(story => {
+        const over = story.real > story.estimated;
+        return (
+          <ProgressBar
+            key={story.id}
+            max={over ? story.real : story.estimated}
+            value={over ? story.estimated : story.real}
+            over={over}
+          />
+        );
+      })}
+    </Sprint>)
+}
+
 export default class App extends React.Component {
   constructor() {
     super();
@@ -78,22 +94,42 @@ export default class App extends React.Component {
           },
         ],
       },
+      {
+        id: 3,
+        name: 'SP3',
+        userStories: [
+          {
+            id: 102,
+            estimated: 4.5,
+            real: 5.0,
+            complex: 3,
+          },
+          {
+            id: 103,
+            estimated: 8.5,
+            real: 6.0,
+            complex: 5,
+          },
+        ],
+      },
     ];
+
+    const renderItems = [];
+
+    for (let i = 0; i < sprints.length; i+=2) {
+      const left = sprints[i];
+      const right = sprints[i+1];
+
+      renderItems.push(
+        <Card key={i}>
+        {left && <CardItem {...left} />}
+        {right && <CardItem {...right} />}
+      </Card>);
+    }
 
     return (
       <div className="home">
-        <Card user="User">
-          {sprints.map((sp, index) => (
-            <Sprint key={sp.id} name={sp.name} date="February 20">
-              {sp.userStories.map(story => {
-                const over = story.real > story.estimated;
-                return (
-                  <ProgressBar key={story.id} max={over ? story.real : story.estimated} value={over ? story.estimated : story.real} over={over} />
-                );
-              })}
-            </Sprint>
-          ))}
-        </Card>
+        {renderItems}
       </div>
     );
   }
