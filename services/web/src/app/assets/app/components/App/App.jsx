906019e68306c9f29914cabca504756400c39c0b
Rodolfo Martínez <rodolfo@agavelab.com>
2018-06-03 05:47:59 -0500

    Implemented sprints

--- services/web/src/app/assets/app/components/App/App.jsx
+++ services/web/src/app/assets/app/components/App/App.jsx
@@ -3,6 +3,7 @@
 import ProgressBar from '../ProgressBar';
 import Card from '../Card';
 import Login from '../Login';
+import Sprint from '../Sprint';
 
 export default class App extends React.Component {
   constructor() {
@@ -40,50 +41,60 @@ export default class App extends React.Component {
   render() {
     const { isLogin } = this.state;
 
-    const userStories = [
+    const sprints = [
       {
         id: 1,
-        estimated: 4.5,
-        real: 5.0,
-        complex: 3,
+        name: 'SP1',
+        userStories: [
+          {
+            id: 100,
+            estimated: 4.5,
+            real: 5.0,
+            complex: 3,
+          },
+          {
+            id: 101,
+            estimated: 8.5,
+            real: 6.0,
+            complex: 5,
+          },
+        ],
       },
       {
         id: 2,
-        estimated: 8.5,
-        real: 6.0,
-        complex: 5,
+        name: 'SP2',
+        userStories: [
+          {
+            id: 102,
+            estimated: 4.5,
+            real: 5.0,
+            complex: 3,
+          },
+          {
+            id: 103,
+            estimated: 8.5,
+            real: 6.0,
+            complex: 5,
+          },
+        ],
       },
     ];
 
-    if (!isLogin) {
-      return <Login onSubmit={this.handleSubmit} />;
-    } else {
-      return (
-        <div className="home">
-          <Card>
-            <div>
-              {userStories.map(story => {
-                if (story.real > story.estimated) {
-                  return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} over />;
-                } else {
-                  return <ProgressBar key={story.id} max={story.estimated * 10} value={story.real * 10} />;
-                }
+    return (
+      <div className="home">
+        <Card user="User">
+          {sprints.map((sp, index) => (
+            <Sprint key={sp.id} name={sp.name} date="February 20">
+              {sp.userStories.map(story => {
+                const over = story.real > story.estimated;
+                return (
+                  <ProgressBar key={story.id} max={over ? story.real : story.estimated} value={over ? story.estimated : story.real} over={over} />
+                );
               })}
-            </div>
-          </Card>
-          <Card>
-            <div>
-              {userStories.map(story => {
-                if (story.real > story.estimated) {
-                  return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} over />;
-                } else {
-                  return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} />;
-                }
-              })}
-            </div>
-          </Card>
-        </div>
-      );
-    }
+            </Sprint>
+          ))}
+        </Card>
+      </div>
+    );
   }
 }
