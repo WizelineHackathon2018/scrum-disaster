ea2d722ace0a4a8d9ee9d27c76832f3655d804bb
LuisAlfredoMtz <luisalfredomtz@hotmail.com>
2018-06-03 09:15:24 -0500

    login

--- services/web/src/app/assets/app/components/Login/Login.jsx
+++ services/web/src/app/assets/app/components/Login/Login.jsx
@@ -1,21 +1,34 @@
-function Login(props) {
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <div className='bg-login'>
-      <div className='bx-form'>
-        <div className="wrapper-login">
-          <div className="title">
-            <span>Inicia Sesión</span>
-          </div>
-          <div>
-            <form className="style-form">
-                <span>Email:</span> <input className="input-style" type="text" name="email" /> <br />
-                <span>Contraseña:</span><input className="input-style" type="password" name="password" />
-            </form>
+  handleSubmit(event) {
+    console.log('EVENT', event)
+    fetch(`http://localhost:3000/login`)
+           .then(result=> {
+             console.log('RESULT', result)
+    });
+  }
+
+  render() {
+    return (
+      <div className='bg-login'>
+        <div className='bx-form'>
+          <div className="wrapper-login">
+            <div className="title">
+              <span>Inicia Sesión</span>
+            </div>
+            <div>
+              <form className="style-form" onSubmit={this.handleSubmit}>
+                  <span>Email:</span> <input className="input-style" type="text" name="email" /> <br />
+                  <span>Contraseña:</span><input className="input-style" type="password" name="password" />
+                  <button className="btn-primary">Iniciar</button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>);
+      </div>);
+  }
 }
-
-export default Login;
