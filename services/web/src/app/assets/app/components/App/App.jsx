de8d41c32af812e5fee8b99204e9497c03f59442
Rodolfo Martínez <rodolfo@agavelab.com>
2018-06-03 10:36:17 -0500

    Config login

--- services/web/src/app/assets/app/components/App/App.jsx
+++ services/web/src/app/assets/app/components/App/App.jsx
@@ -1,11 +1,38 @@
 /* global React */
+/* global API_HOST */
 
 import ProgressBar from '../ProgressBar';
 import Card from '../Card';
 import Login from '../Login';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      isLogin: false,
+      result: null,
+    };
+  }
+
+  handleSubmit() {
+    console.log('handleSubmit...');
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: 'rodolfo@agavelab.com', password: 'v2c47mk7jd' }),
+    })
+      .then(res => res.json())
+      .then(res => this.setState({ isLogin: true, result: res }));
+  }
+
   render() {
+    const { isLogin } = this.state;
+
     const userStories = [
       {
         id: 1,
@@ -21,47 +48,35 @@ export default class App extends React.Component {
       },
     ];
 
-    const isLogin = true;
-
-    if(isLogin) {
-        return (
-          <Login  />
-        )
+    if (!isLogin) {
+      return <Login onSubmit={this.handleSubmit} />;
+    } else {
+      return (
+        <div className="home">
+          <Card>
+            <div>
+              {userStories.map(story => {
+                if (story.real > story.estimated) {
+                  return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} over />;
+                } else {
+                  return <ProgressBar key={story.id} max={story.estimated * 10} value={story.real * 10} />;
+                }
+              })}
+            </div>
+          </Card>
+          <Card>
+            <div>
+              {userStories.map(story => {
+                if (story.real > story.estimated) {
+                  return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} over />;
+                } else {
+                  return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} />;
+                }
+              })}
+            </div>
+          </Card>
+        </div>
+      );
     }
-
-    return (
-      <div className='home'>
-        <Card>
-          <div>
-            {userStories.map(story => {
-              if (story.real > story.estimated) {
-                return <ProgressBar
-                          key={story.id}
-                          max={story.real * 10}
-                          value={story.estimated * 10}
-                          over />;
-              } else {
-                return <ProgressBar
-                          key={story.id}
-                          max={story.estimated * 10}
-                          value={story.real * 10} />;
-              }
-            })}
-          </div>
-        </Card>
-        <Card>
-          <div>
-            {userStories.map(story => {
-              if (story.real > story.estimated) {
-                return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} over />;
-              } else {
-                return <ProgressBar key={story.id} max={story.real * 10} value={story.estimated * 10} />;
-              }
-            }
-          )}
-          </div>
-        </Card>
-      </div>
-    );
   }
 }
--- services/web/src/app/assets/app/components/Login/Login.jsx
+++ services/web/src/app/assets/app/components/Login/Login.jsx
@@ -1,34 +1,25 @@
 export default class Login extends React.Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    console.log('EVENT', event)
-    fetch(`http://localhost:3000/login`)
-           .then(result=> {
-             console.log('RESULT', result)
-    });
-  }
-
   render() {
+    const { onSubmit } = this.props;
+
     return (
-      <div className='bg-login'>
-        <div className='bx-form'>
+      <div className="bg-login">
+        <div className="bx-form">
           <div className="wrapper-login">
             <div className="title">
               <span>Inicia Sesión</span>
             </div>
             <div>
-              <form className="style-form" onSubmit={this.handleSubmit}>
-                  <span>Email:</span> <input className="input-style" type="text" name="email" /> <br />
-                  <span>Contraseña:</span><input className="input-style" type="password" name="password" />
-                  <button className="btn-primary">Iniciar</button>
+              <form method="post" className="style-form" onSubmit={onSubmit}>
+                <span>Email:</span> <input className="input-style" type="text" name="email" /> <br />
+                <span>Contraseña:</span>
+                <input className="input-style" type="password" name="password" />
+                <button className="btn-primary">Iniciar</button>
               </form>
             </div>
           </div>
         </div>
-      </div>);
+      </div>
+    );
   }
 }
